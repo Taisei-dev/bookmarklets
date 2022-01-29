25eb44c94a705717fc19b76622fc53c8ddae751d
誠 <87258462+Taisei-dev@users.noreply.github.com>
2022-01-29 16:28:27 +0900

    Create 規制.js

---
+++ 規制.js
@@ -0,0 +1,2 @@
+javascript:
+window.location.replace('http://isgcblock.iss.netstar-inc.com:8080/block.cgi?urldata='+encodeURIComponent(location.href)+'&ctgnum=0801&uid=36311&gid=86&rcode=1,2,-2&cip=0.0.0.0');
